40a732ee74c53c1c285d3fde6747bfe38b40d485
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-28 09:55:08 +0100

    update main.go

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
 	http.HandleFunc("/", handleRoute)
-	http.ListenAndServe(":3000", nil)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
 
 func handleRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "My not-so-first REST API")
+	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+	}
+	fmt.Fprint(w, message)
 }
